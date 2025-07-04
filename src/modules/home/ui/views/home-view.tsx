6336bf1144ba1b3573421f3a29dccdced3dfc780
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-07-04 19:27:28 +0530

    Agents-Setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({text: "world"}));
-  
+
 
   return (
-  <div className="p-4 flex flex-col gap-y-4">
-    {data?.greeting}
+  <div>
+    Home View
   </div>
   );
 };
