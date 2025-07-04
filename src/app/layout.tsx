3fd8b559d596f81dccf6b40cb434e7755e35a88f
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-07-04 16:53:09 +0530

    trpc-setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Import } from "lucide-react";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const inter = Inter({
- 
   subsets: ["latin"],
 });
 
@@ -18,12 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </TRPCReactProvider>
   );
 }
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,26 +1,15 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
 
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 export const HomeView = () => {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-  if (!session) {
-    return <p>Loading...</p>;
-  }
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({text: "world"}));
+  
+
   return (
-    <div className="p-4 flex flex-col gap-y-4">
-      <p>Logged in as {session.user.name}</p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: { onSuccess: () => router.push("/sign-in") },
-          })
-        }
-      >
-        Sign out
-      </Button>
-    </div>
+  <div className="p-4 flex flex-col gap-y-4">
+    {data?.greeting}
+  </div>
   );
 };
