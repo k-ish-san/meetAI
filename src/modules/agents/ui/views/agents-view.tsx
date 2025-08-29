9bbc26928ba7488a7722cbcea8b5a22b1a80ffb6
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-08-29 15:08:56 +0530

    responsive dialogs

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,18 +4,33 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { Button } from "@/components/ui/button";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      <ResponsiveDialog
+        title="Responsive Test"
+        description="Responsive description"
+        open
+        onOpenChange={() => {}}
+      >
+        <Button>
+          Some Action
+        </Button>
+      </ResponsiveDialog>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => (
   <LoadingState title="Loading agents" description="Please wait..." />
 );
-
 
 export const AgentsViewError = () => (
   <ErrorState
